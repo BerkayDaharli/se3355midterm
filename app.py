e1a4b62b7c99bdf9a1e8291fe9553210b242f3eb
Berkay <165518153+BerkayDaharli@users.noreply.github.com>
2024-04-23 18:46:11 +0300

    Added db tables

--- app.py
+++ app.py
@@ -1,12 +1,62 @@
-import sqlite3 as sql
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, redirect, render_template, request, url_for
+from sqlalchemy_utils import database_exists, create_database
 
 app = Flask(__name__)
 
+username = "postgres"
+password = "postgres"
+host = "localhost"
+port = "5432"
+db_name = "se3355mt_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{username}:{password}@{host}:{port}/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-def initDB():
-    conn = sql.connect('database.db')
-    print("Opened database successfully")
+db = SQLAlchemy(app)
+
+
+class Campaign(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.String(255), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<Campaign {self.title}>'
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    category = db.Column(db.Text, nullable=False)
+    colors = db.relationship('ProductColor', backref='product')
+
+
+class ProductColor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    color = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    image_url = db.Column(db.Text, nullable=False)
+
+
+def setup_db():
+    with app.app_context():
+        if not database_exists(db.engine.url):
+            create_database(db.engine.url)
+        db.create_all()
+
+        if Campaign.query.count() == 0:
+            campaigns = [
+                Campaign(title='Summer Sale', description='Up to 50% off on summer items!', image_url=''),
+                Campaign(title='Winter Wonders', description='Explore cozy winter gear!', image_url=''),
+                Campaign(title='Spring Collection', description='Fresh looks for spring!', image_url=''),
+                Campaign(title='Autumn Arrivals', description='Get ready for the cool autumn breeze.', image_url=''),
+                Campaign(title='Back to School', description='Everything you need for school.', image_url='')
+            ]
+            db.session.add_all(campaigns)
+            db.session.commit()
 
 
 @app.route("/")
@@ -15,5 +65,5 @@ def home():
 
 
 if __name__ == "__main__":
-    initDB()
+    setup_db()
     app.run(debug=True)
